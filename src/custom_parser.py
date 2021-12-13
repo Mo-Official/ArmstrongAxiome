e8106260ee7b1496c63c90eba1681948811def3f
Mo-Official <inf19151@lehre.dhbw-stuttgart.de>
2021-12-13 11:03:14 +0100

    formatting

--- src/custom_parser.py
+++ src/custom_parser.py
@@ -18,28 +18,28 @@ from re import compile
 from src.models import Dependacy, Side, Attribute
 
 
-STRICT_MODE = True # Turn off to stop raising errors
-
+STRICT_MODE = True  # Turn off to stop raising errors
 
 
 def parse_attributes(input_string) -> List[Attribute]:
     """Parse an input string to find attributes.
     Input string must be in the following Form:
-    "{A,B,C,D,E,F,G}"  
+    "{A,B,C,D,E,F,G}"
     returns a list of Attributes object
     """
     s = "{A,B,C,D,E,F,G}"
+
     def validate_input(input_string):
-            unvalid_input = compile("\{([A-Z]\,)*([A-Z])\}")
-            search_res, _ = unvalid_input.subn("", input_string)
-            if search_res:
-                if STRICT_MODE:
-                    raise error(f"Input {search_res} is invalid")
-                else:
-                    print(f"Skipping ({input_string}) because it's invalid")
-                    return False
+        unvalid_input = compile("\{([A-Z]\,)*([A-Z])\}")
+        search_res, _ = unvalid_input.subn("", input_string)
+        if search_res:
+            if STRICT_MODE:
+                raise error(f"Input {search_res} is invalid")
             else:
-                return True
+                print(f"Skipping ({input_string}) because it's invalid")
+                return False
+        else:
+            return True
 
     if validate_input(input_string):
         return [Attribute(att) for att in input_string[1:-1].split(",")]
@@ -51,6 +51,7 @@ def parse_dependancy(input_string) -> Dependacy:
     [A-Z]+->[A-Z]+
     returns a Dependacy object
     """
+
     def validate_input(input_string):
         unvalid_input = compile("[A-Z]+\-\>[A-Z]+")
         search_res, _ = unvalid_input.subn("", input_string)
@@ -62,14 +63,14 @@ def parse_dependancy(input_string) -> Dependacy:
                 return False
         else:
             return True
-    
+
     # TODO: Check if each side has known attributes
 
     def parse_left_side(input_string):
         right_side_prg = compile("[A-Z]+\-\>")
         right_side_args = right_side_prg.findall(input_string)[0][:-2]
         return Side([Attribute(arg) for arg in right_side_args])
-    
+
     def parse_right_side(input_string):
         left_side_prg = compile("\-\>[A-Z]+")
         left_side_args = left_side_prg.findall(input_string)[0][2:]
@@ -78,12 +79,22 @@ def parse_dependancy(input_string) -> Dependacy:
     if validate_input(input_string):
         return Dependacy(parse_left_side(input_string), parse_right_side(input_string))
 
+
 if __name__ == "__main__":
     import pprint
-    attribut_menge="{A,B,C,D}"
-    global attributes
+
+    attribut_menge = "{A,B,C,D}"
     attributes = parse_attributes(attribut_menge)
-    dependacies_menge = ["A->D","BD->ED","BC->H","D->A","H->DEF","F->B","E->C","G->HF"]
+    dependacies_menge = [
+        "A->D",
+        "BD->ED",
+        "BC->H",
+        "D->A",
+        "H->DEF",
+        "F->B",
+        "E->C",
+        "G->HF",
+    ]
     dependacies = []
     for d in dependacies_menge:
         dependacies.append(parse_dependancy(d))
